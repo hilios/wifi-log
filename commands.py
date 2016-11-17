49ac8a0b90a8b74b973f60433cbea13f58fb7fda
Edson Hilios <edson.hilios@nubank.com.br>
2016-11-17 21:39:19 -0200

    Better error handling

--- commands.py
+++ commands.py
@@ -6,27 +6,36 @@ import sys
 import time
 from datetime import date
 
+if os.geteuid() != 0:
+    print >> sys.stderr, "You need root permissions to run this program."
+    sys.exit(1)
+
 # Prepare the environment
 try:
     os.makedirs('/var/lib/apc/log')
 
 except OSError as err:
     if not err.strerror in ['File exists']:
-        print(dir(err))
-        sys.exit(1)
+        raise err
 
 
 PID_FILE = lockfile.FileLock('/var/run/apc.pid')
 LOG_FILE = open('/var/lib/apc/log/%s.log' % date.today().isoformat(), 'w+')
 
 
-def start(detached):
+def start(detached, url=None):
+    if PID_FILE.is_locked():
+        print >> sys.stderr, "Already running in, stop it before try again."
+        sys.exit(1)
+
     print('Starting...')
+    
     context = daemon.DaemonContext(detach_process=detached, umask=0o002,
         working_directory='/var/lib/apc',
         stdout=LOG_FILE if detached else sys.stdout,
         stderr=LOG_FILE if detached else sys.stderr,
-        pidfile=PID_FILE)
+        pidfile=PID_FILE,
+    )
     context.signal_map = {
         # signal.SIGUSR1: reload_program_config,
         signal.SIGHUP:      'terminate',
@@ -34,9 +43,12 @@ def start(detached):
     }
     context.files_preserve = [LOG_FILE]
     with context:
-        while True:
-            print("Hello, world!")
-            time.sleep(5)
+        try:
+            while True:
+                print("Hello, world!")
+                time.sleep(5)
+        except KeyboardInterrupt:
+            print("Stopping now!")
 
 
 def stop(**kwargs):
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import argparse
 import commands as cmd
 import daemon
-import argparse
+import os
 import sys
 
 
@@ -9,6 +10,7 @@ subcmd = parser.add_subparsers()
 
 start = subcmd.add_parser('start')
 start.set_defaults(func=cmd.start)
+start.add_argument('--url', '-u', action='store', default=None)
 start.add_argument('--detached', '-d', action='store_true')
 
 stop = subcmd.add_parser('stop')
