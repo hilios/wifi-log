1d65eb039e1230bf4d208504a9f7e50475112420
Edson Hilios <edson.hilios@nubank.com.br>
2016-11-19 17:51:58 -0200

    Basic scapy config for probe requests

--- monitor/sniffer.py
+++ monitor/sniffer.py
@@ -1,22 +1,25 @@
 import logging
+import os
 import sys
-
 
 log = logging.getLogger('sniffer')
 
+if os.geteuid() != 0:
+    print >> sys.stderr, "You need root permissions to run this program."
+    sys.exit(1)
 
 try:
-    from scapy.all import *
+    from scapy.all import sniff, Dot11Beacon, Dot11ProbeResp
 except ImportError:
     log.error("Your system does not support sniffing")
     sys.exit(1)
 
 
-def process_pkt(pkt):
-    bssid = pkt[Dot11].addr3
-    log.info("")
+def start(interface):
+    "Start monitoring the given interface for probe requests."
+    def pkt_handler(pkt):
+        mac_address = pkt.addr2
+        log.info(mac_address)
 
-
-def start():
-    sniff(iface=interface, prn=process_pkt, store=False,
+    sniff(iface=interface, prn=pkt_handler, store=False,
         lfilter=lambda p: (Dot11Beacon in p or Dot11ProbeResp in p))
