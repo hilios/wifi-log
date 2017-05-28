c3af62fca4fb0212cccfe7985643781a1838261f
Edson Hilios <edson.hilios@gmail.com>
2017-05-28 02:31:08 -0300

    Improve setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 from setuptools import setup, find_packages
+from os import path
 
+here = path.abspath(path.dirname(__file__))
 
 setup(
     name="wifi-apc",
-    version="0.0.2",
-    packages=find_packages('.'),
+    version="0.3",
+    packages=find_packages(here),
     include_package_data=True,
     zip_safe=True,
     install_requires=[
         'scapy',
         'zmq',
     ],
-    entry_points='''
-        [console_scripts]
-        wifi-apc=main:run
-    ''',
+    data_files=[
+        ('/etc/systemd/system', ['resources/wifi-apc.service'])
+    ],
+    entry_points={
+        'console_scripts': [
+            'wifi-apc=main:run',
+        ],
+    },
 )
