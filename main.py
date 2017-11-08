070da80230f6340b520ddee3a0975530fe6662df
Edson Hilios <edson.hilios@99taxis.com>
2017-11-08 16:39:38 -0200

    Fix log extension

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def signal_handler(signal_code, frame):
 
 def run():
     "Parses the CLI and run the application"
-    current_date = datetime.datetime.utcnow().strftime('%Y-%m-%d.log')
+    current_date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', '-v', action='store_true')
