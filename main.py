d373ad385a61288681cf7159ecdcf7164feea2b4
Edson Hilios <edson.hilios@99taxis.com>
2017-11-13 18:37:47 -0200

    Use watched file handler

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def run():
     # Configure log utility
     logging.basicConfig(level=logging.DEBUG, format=DEFAULT_FMT)
     # Rotating file log
-    logfile = logging.FileHandler(args.log_file)
+    logfile = logging.handlers.WatchedFileHandler(args.log_file)
     logfile.setLevel(logging.INFO)
     logfile.setFormatter(logging.Formatter(DEFAULT_FMT))
     logging.getLogger('').addHandler(logfile)
